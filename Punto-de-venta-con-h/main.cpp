f614d9a4b021e4c197e5582e7282541389f3ee8e
aritacras <106627165+aritacras@users.noreply.github.com>
2022-07-13 14:32:04 -0600

    Add files via upload

---
+++ Punto-de-venta-con-h/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "MENU.h"
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+extern void menu();
+
+int main(int argc, char** argv)
+{
+	menu();
+	
+	return 0;
+	
+}
+
